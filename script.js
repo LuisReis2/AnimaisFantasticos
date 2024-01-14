938429128170dd93ea540529aa13d02215aeb297
Luis Henrique <luishcardoso228@gmail.com>
2024-01-13 21:22:13 -0300

    scroll-links-internos

--- script.js
+++ script.js
@@ -49,3 +49,23 @@ function showScroll() {
 }
 initScrollView()
 
+
+function initScrollTo(){
+  const linksInternos = document.querySelectorAll('[href^="#"]');
+  if(linksInternos){
+    linksInternos.forEach((link)=>{
+      link.addEventListener('click', (event)=>{
+        event.preventDefault();
+        const href = link.getAttribute('href');
+        const section = document.querySelector(href)
+        console.log(section)
+         section.scrollIntoView({
+           behavior: 'smooth',
+           block: 'start'
+         })
+      })
+    })
+  }
+}
+
+initScrollTo()
